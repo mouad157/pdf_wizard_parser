17f2d8dbe8fa4404ed3a0dfaa0d32390f0b06b07
mouad157 <94315381+mouad157@users.noreply.github.com>
2024-09-11 12:13:52 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,21 +1,24 @@
 import streamlit as st
-import streamlit.components.v1 as stc
 from sentence_transformers import SentenceTransformer, util
 from nltk.tokenize import word_tokenize
 from streamlit_pdf_viewer import pdf_viewer
 import streamlit_scrollable_textbox as stx
 import torch
 import pdfplumber
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+stpwrd = stopwords.words('english')
 import re
 
-def parse(pdfu):
+def parse(pdfu,chunk_size):
     paginas = []
     with pdfplumber.open(pdfu) as pdf:
         total = ""
 
         for i in range(len(pdf.pages)):
             page = pdf.pages[i]
-            pagei = page.within_bbox((0, page.height*0.18 , page.width, page.height-page.height*0.18))
+            pagei = page.within_bbox((0, page.height*0.05 , page.width, page.height-page.height*0.05))
             text = pagei.extract_text(layout=True)
             paginas.append(text)
             total = total + text
@@ -23,30 +26,55 @@ def parse(pdfu):
     graph = []
     texts1 = re.sub(r'\n\s+\n\s+\n', '\n\n\n', total)
     texts = re.sub(r' +', ' ', texts1)
-    tex = ""
-    for j in range(len(texts)-1):
-
-        if texts[j] =="\n" and texts[j+1] == "\n" and len(word_tokenize(tex)) >= 400:
-            graph.append(tex)
-            tex= ""
+    # tex = ""
+    graph3= []
+    original = ""
+    sentences = texts.split("\n")
+    for i in range(len(sentences)):
+        p=True
+        tokenized = word_tokenize(sentences[i])
+        if len(tokenized) == 0:
+            continue
+        words = [word for word in tokenized if word not in stpwrd]
+        #print(words)
+        if len(words) <= 1:
+            p= False
+        for l in words:
+            if l[0].isupper() or l[0].isdigit():
+                continue
+            else:
+                p=False
+                break
+        if p:
+            tokens = word_tokenize(original)
+            if len(tokens) >= chunk_size:
+                graph3.append(original)
+                original = ""
+            original = original +"\n"+ sentences[i]+"\n"
+            
         else:
-            tex = tex + texts[j]
-    return graph
+            original = original + sentences[i]
+        if i == len(sentences) -1:
+            graph3.append(original)
+
+    return graph3
 
 
 def search(model, question, texts):
     query_embedding = model.encode(question)
     passage_embeddings = model.encode(texts)
     similarity = util.pytorch_cos_sim(query_embedding, passage_embeddings)
-    top = torch.topk(similarity,5)
+    top = torch.topk(similarity,3)
     indices = top.indices.squeeze().tolist()
     out = [texts[i] for i in indices]
-    # for k in out:
-    #     terc = serach
     return indices, out
 
 
 def main():
+    st.set_page_config(
+    page_title="Wizard parser",
+    page_icon="🧊",
+    layout="wide")
     JOB_HTML_TEMPLATE = """
     <div style='color:#444'>
     <h4>{}</h4>
@@ -61,20 +89,20 @@ def main():
     
     # Nav  Search Form
     with st.form(key='searchform'):
-        nav1,nav2,nav3 = st.columns([5,3,2])
-
+        nav1,nav2,nav3,nav4 = st.columns([5,3,2,2])
         with nav1:
             search_term = st.text_input("What is your question")
         with nav2:
             file =st.file_uploader("Upload a PDF file", type="pdf")
-            
         with nav3:
+            chunk_size = st.text_input("Chunk_size")
+        with nav4:
             st.text("Search")
             submit_search = st.form_submit_button(label='Search')
     
     if submit_search:
         with st.spinner('Parsing the PDF file. Please wait.'):
-            texts = parse(file)
+            texts = parse(file,int(chunk_size))
         st.success("pdf is parsed!")
         st.success("You searched for: {}".format(search_term))
         content,mtd = search(model,search_term,texts)
@@ -85,9 +113,7 @@ def main():
                 width=700,height=600)
         for i in mtd:
             body = i
-            # st.markdown(JOB_HTML_TEMPLATE.format(body.split('\n')[0],body),
-            # 		unsafe_allow_html=True)
-            with st.expander(body.split('\n')[2]):
-                stx.scrollableTextbox(body[2:],height = 300)
+            with st.expander(body.split('\n')[1]):
+                stx.scrollableTextbox(body[1:],height = 300)
 if __name__ == '__main__':
 	main()
--- util.py
+++ util.py
@@ -3,10 +3,13 @@ import torch
 import argparse
 import pdfplumber
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
 import re
+nltk.download('stopwords')
+stpwrd = stopwords.words('english')
 
-
-def wizard_parser(file_path,question,chunk_size = 400, k=5):
+def wizard_parser(file_path,question,chunk_size = 300, k=5):
 
     #This function takes a pdf file path, a question, chunk size and a integer k and returns a list of k useful chunks from 
     # the pdf file that are at least in the size of chunk_size
@@ -25,21 +28,45 @@ def wizard_parser(file_path,question,chunk_size = 400, k=5):
     graph = []
     texts1 = re.sub(r'\n\s+\n\s+\n', '\n\n\n', total)
     texts = re.sub(r' +', ' ', texts1)
-
-    tex = ""
-    for j in range(len(texts)-1):
-
-        if texts[j] =="\n" and texts[j+1] == "\n" and len(word_tokenize(tex)) >= chunk_size:
-            graph.append(tex)
-            tex= ""
+    graph2 = []
+    graph3= []
+    clean = ""
+    original = ""
+    sentences = texts.split("\n")
+    for i in range(len(sentences)):
+        p=True
+        tokenized = word_tokenize(sentences[i])
+        if len(tokenized) == 0:
+            continue
+        words = [word for word in tokenized if word not in stpwrd]
+        print(words)
+        if len(words) <= 1:
+            p= False
+        for l in words:
+            if l[0].isupper() or l[0].isdigit():
+                continue
+            else:
+                p=False
+                break
+        if p:
+            tokens = word_tokenize(original)
+            if len(tokens) >= chunk_size:
+                graph3.append(original)
+                original = ""
+            original =  original +"\n"+ sentences[i]+"\n"
+            
         else:
-            tex = tex + texts[j]
+            original = original + sentences[i]
+        if i == len(sentences) -1:
+            graph3.append(original)
 
     model = SentenceTransformer("multi-qa-mpnet-base-cos-v1")
     query_embedding = model.encode(question)
-    passage_embeddings = model.encode(graph)
+    passage_embeddings = model.encode(graph3)
     similarity = util.pytorch_cos_sim(query_embedding, passage_embeddings)
-    top = torch.topk(similarity,5)
+    top = torch.topk(similarity,3)
     indices = top.indices.squeeze().tolist()
-    tert = [graph[i] for i in indices]
-    return tert
+    tert = [graph3[i] for i in indices]
+    return tert,len(graph3)
+if __name__ == '__main__':
+	print(wizard_parser("./wdg.pdf", "how to setup an access point"))
